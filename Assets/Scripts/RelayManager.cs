8d5b129523803b44f701fcd955500ec5fda6e0df
lstevensN <lstevens@student.neumont.edu>
2025-04-08 16:53:22 -0600

    Added try-catch blocks
    for safety!

--- Assets/Scripts/RelayManager.cs
+++ Assets/Scripts/RelayManager.cs
@@ -38,20 +38,40 @@ public class RelayManager : MonoBehaviour
 
     private async Task<string> StartHostWithRelay(int maxConnections = 3)
     {
-        Allocation allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections);
+        Allocation allocation;
+        string joinCode;
+
+        try { allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections); }
+        catch
+        {
+            Debug.LogError("Failed to create allocation");
+            throw;
+        }
 
         RelayServerData relayServerData = AllocationUtils.ToRelayServerData(allocation, "udp");  // Can change connectionType to dtls
 
         NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(relayServerData);
 
-        string joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+        try { joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId); }
+        catch
+        {
+            Debug.LogError("Failed to get join code");
+            throw;
+        }
 
         return NetworkManager.Singleton.StartHost() ? joinCode : null;
     }
 
     private async Task<bool> StartClientWithRelay(string joinCode)
     {
-        JoinAllocation joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode);
+        JoinAllocation joinAllocation;
+
+        try { joinAllocation = await RelayService.Instance.JoinAllocationAsync(joinCode); }
+        catch
+        {
+            Debug.LogError("Failed to create join allocation");
+            throw;
+        }
 
         RelayServerData relayServerData = AllocationUtils.ToRelayServerData(joinAllocation, "udp");  // Can change connectionType to dtls
 
